9f9cd3e388b3a4fb22a9c436b693ecea8d856507
Melquiades Fidelis <melquiades013fidelis@gmail.com>
2022-05-23 08:37:23 -0300

    implementação do metodo delete

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,8 @@
-import { FotoService } from './../services/foto.service';
 import { Component } from '@angular/core';
+import { FotoService, Foto } from '../services/foto.service';
+import { ActionSheetController } from '@ionic/angular';
+import { PopoverComponent } from './../popover/popover.component';
+import { PopoverController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +11,34 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor(public fotoService: FotoService) {}
+  constructor(public fotoService: FotoService, public actionSheetController: ActionSheetController, 
+              public popoverController: PopoverController) { }
+
+  async presentActionSheet(i : number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'O que deseja fazer?',
+      buttons: [{
+        text: 'Deletar',
+        icon: 'trash',
+        handler: () => {
+          this.fotoService.deleteFoto(i);
+        }
+        }, {
+        text: 'Cancelar',
+        icon: 'close',
+        role: 'cancel'
+      }]
+    });
+    await actionSheet.present();
+    } 
+
+    async presentPopover(ev: any) {
+      const popover = await this.popoverController.create({
+        component: PopoverComponent,
+        event: ev,
+        translucent: true
+      });
+      return await popover.present();
+    }
 
 }
