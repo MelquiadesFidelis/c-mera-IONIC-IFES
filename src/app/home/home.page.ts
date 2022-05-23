7cbf4c33f90a596ed61feaf99a423a3d4d297b8f
Melquiades Fidelis <melquiades013fidelis@gmail.com>
2022-05-23 07:57:29 -0300

    API Câmera Cordova implementado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { FotoService } from './../services/foto.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,6 +8,6 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(public fotoService: FotoService) {}
 
 }
