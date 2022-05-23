e0ed7f5c810d00b6c34b78d3bdad94ff3f05c2c0
Melquiades Fidelis <melquiades013fidelis@gmail.com>
2022-05-23 08:11:29 -0300

    Tratamento interface da foto

--- src/app/services/foto.service.ts
+++ src/app/services/foto.service.ts
@@ -9,13 +9,28 @@ const { Camera } = Plugins;
 })
 export class FotoService {
 
+  public fotos: Foto[] = [];
+
   constructor() { }
 
   public async addNewToGallery() {
     const capturedPhoto = await Camera.getPhoto({
-        resultType: CameraResultType.Uri,
-        source: CameraSource.Camera,
-        quality: 100
+    resultType: CameraResultType.Uri,
+    source: CameraSource.Camera,
+    quality: 100
     });
+    this.fotos.unshift({
+      webviewPath: capturedPhoto.webPath
+    });     
+  }
+
+  public deleteFoto(index : number) {
+    this.fotos.splice(index, 1);
+  }
+   
 }
+
+export interface Foto {
+  webviewPath: string;
+  base64?: string;
 }
